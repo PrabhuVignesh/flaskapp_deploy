e7b341f3a5a841cb4b11e4da5eb77d60c2a6b939
Kumar <prabhukumar@kpmg.com>
2016-11-23 12:40:29 +0530

    change

--- flaskapp.py
+++ flaskapp.py
@@ -12,13 +12,13 @@ def count_me(input_str):
   return middle_str
 
 
-@app.route('/auth')
+@app.route('/auth/<input_str>')
 def auth(input_str):
   middle_str = "you have entered this '" + input_str +"' in URL"
   return middle_str
 
-@app.route('/basicauth')
-def basic_auth(input_str):
+@app.route('/basicauth/<username>/<password>')
+def basic_auth(username,password):
   authed_jira = JIRA(basic_auth=('vishalkumar2', '$Jira1353'))
   return middle_str
 
