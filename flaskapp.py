3fe2561e5b1f4326e2cb0f8fd640e961ed26a69e
Kumar <prabhukumar@kpmg.com>
2016-11-23 12:03:04 +0530

    change

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from jira import JIRA
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +10,18 @@ def hello_world():
 def count_me(input_str):
   middle_str = "you have entered this '" + input_str +"' in URL"
   return middle_str
+
+
+@app.route('/auth')
+def count_me(input_str):
+  middle_str = "you have entered this '" + input_str +"' in URL"
+  return middle_str
+
+@app.route('/basicauth')
+def count_me(input_str):
+  authed_jira = JIRA(basic_auth=('vishalkumar2', '$Jira1353'))
+  return middle_str
+
 
 
 if __name__ == '__main__':
